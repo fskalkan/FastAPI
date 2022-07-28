22e9c6c91dbde83abf61b307046d3cc87d40c919
= <=>
2022-07-29 00:40:09 +0300

    Finished

--- main.py
+++ main.py
@@ -1,15 +1,64 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query, HTTPException, status
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-inventory = {
-        1: {
-            "name": "Milk",
-            "price": 4.99,
-            "brand": "Regular"
-        }
-    }
+class Item(BaseModel):
+    name:str
+    price:float
+    brand:Optional[str]=None
+
+class UpdateItem(BaseModel):
+    name:Optional[str]=None
+    price:Optional[float]=None
+    brand:Optional[str]=None
+
+inventory = {}
 
 @app.get("/get-item/{item_id}")
-def get_item(item_id: int = Path(None, tanım = "görüntülemek istediğiniz öğenin ID'si")):
+def get_item(item_id: int):
     return inventory[item_id]
+
+@app.get("/get-by-name")
+def get_item(name: str):
+    for item_id in inventory:
+        if inventory[item_id].name==name:
+            return inventory[item_id]
+    raise HTTPException(status_code=404)
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        raise HTTPException(status_code=400)
+
+    inventory[item_id]=item
+    return inventory[item_id]
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
+    if item_id not in inventory:
+        raise HTTPException(status_code=404)
+
+    if item.name != None:
+        inventory[item_id].name = item.name
+
+    if item.price != None:
+        inventory[item_id].price = item.price
+
+    if item.brand != None:
+        inventory[item_id].brand = item.brand
+
+    return inventory[item_id]
+
+@app.delete("/delete-item")
+def delete_item(item_id: int):
+    if item_id not in inventory:
+        raise HTTPException(status_code=404)
+
+    del inventory[item_id]
+    return {"Success": "Item deleted!"}
+
+    
+
+
